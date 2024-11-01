554c6dd4467862c7483f05c4435058e654f3140f
csenazmul <csenazmul77@gmail.com>
2024-11-02 01:05:40 +0600

    update leapyear using logical operator with control flow

--- Control Flow/LeapYear.py
+++ Control Flow/LeapYear.py
@@ -1,16 +1,23 @@
 
-def LeapYear(year):
+# def LeapYear(year):
     
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                print(f"This {year} is Leap Year.")
-            else:
-                print(f"This {year} is not Leap Year.")
-        else:
-            print(f"This {year} is Leap Year.")
-    else:
-        print(f"This {year} is not Leap Year.")
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 print(f"This {year} is Leap Year.")
+#             else:
+#                 print(f"This {year} is not Leap Year.")
+#         else:
+#             print(f"This {year} is Leap Year.")
+#     else:
+#         print(f"This {year} is not Leap Year.")
 
 year = int(input("Enter the Year Value: "))
-LeapYear(year)
+# LeapYear(year)
+
+
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    print(f"This {year} is Leap Year.")
+
+else:
+    print(f"This {year} is not Leap Year.")
