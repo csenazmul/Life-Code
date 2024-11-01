e423b855a9e9881075a0bde15660fc56b237a136
csenazmul <csenazmul77@gmail.com>
2024-11-02 00:54:02 +0600

    Program LeapYear control flow nested if using function

---
+++ Control Flow/LeapYear.py
@@ -0,0 +1,16 @@
+
+def LeapYear(year):
+    
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print(f"This {year} is Leap Year.")
+            else:
+                print(f"This {year} is not Leap Year.")
+        else:
+            print(f"This {year} is Leap Year.")
+    else:
+        print(f"This {year} is not Leap Year.")
+
+year = int(input("Enter the Year Value: "))
+LeapYear(year)
