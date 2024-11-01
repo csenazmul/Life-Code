cd15cd1d670c794a051562e2b34424cf68f59e8e
csenazmul <csenazmul77@gmail.com>
2024-11-01 21:43:47 +0600

    update bmicalculator

---
+++ Control Flow/BMICalculator.py
@@ -0,0 +1,36 @@
+
+def bmi_calculator(weight_kg, height_cm):
+    # Convert CM to Meter
+    height_m = height_cm / 100
+
+    print(f"Height Value of (M): {height_m:.2f}")
+
+    # BMI Formula
+    bmi = weight_kg / (height_m ** 2)
+
+    if bmi < 18.5:
+        status = "Under Weight"
+    
+    elif 18.5 <= bmi < 24.9:
+        status = "Normal Weight"
+
+    elif 25 <= bmi < 29.9:
+        status = "Over Weight"
+    # elif bmi>= 30:
+    #     status = "Obese"
+    else:
+        status = "Obese"
+    
+    return bmi, status
+
+
+weight_kg = float(input("Please Enter the Weight Value of (KG): "))
+height_cm = float(input("Please Enter the Height Value of (CM): "))
+
+
+
+bmi, status = bmi_calculator(weight_kg, height_cm)
+
+# print(f"Height Value of (M): {height_m:.2f}")
+print(f"BMI Result: {bmi:.2f}")
+print(f"BMI Status: {status}")
