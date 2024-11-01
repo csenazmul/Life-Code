a8d8d93e12790ed2fc59f1559cdc421a4880a4c1
csenazmul <csenazmul77@gmail.com>
2024-11-01 21:49:23 +0600

    update BMI

--- Control Flow/BMICalculator.py
+++ Control Flow/BMICalculator.py
@@ -3,7 +3,7 @@ def bmi_calculator(weight_kg, height_cm):
     # Convert CM to Meter
     height_m = height_cm / 100
 
-    print(f"Height Value of (M): {height_m:.2f}")
+    # print(f"Height Value of (M): {height_m:.2f}")
 
     # BMI Formula
     bmi = weight_kg / (height_m ** 2)
@@ -21,7 +21,7 @@ def bmi_calculator(weight_kg, height_cm):
     else:
         status = "Obese"
     
-    return bmi, status
+    return bmi, status ,height_m
 
 
 weight_kg = float(input("Please Enter the Weight Value of (KG): "))
@@ -29,8 +29,8 @@ height_cm = float(input("Please Enter the Height Value of (CM): "))
 
 
 
-bmi, status = bmi_calculator(weight_kg, height_cm)
+bmi, status, height_m = bmi_calculator(weight_kg, height_cm)
 
-# print(f"Height Value of (M): {height_m:.2f}")
+print(f"Height Value of (M): {height_m:.2f}")
 print(f"BMI Result: {bmi:.2f}")
 print(f"BMI Status: {status}")
