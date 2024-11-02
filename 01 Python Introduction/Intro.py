352227ce5b0f921f2f68d06bf4f687ed7e3c3aed
csenazmul <csenazmul77@gmail.com>
2024-11-02 21:47:49 +0600

    Create Python Introduction and Basic Syntax and Variable

---
+++ 01 Python Introduction/Intro.py
@@ -0,0 +1,18 @@
+# Hello World Program
+print("Hello World!")
+
+# User Input value
+name = input("What is your name? ")
+print("Welcome, "+name+ "!")
+print(type(name))
+
+# Project 01 Want to user name and age
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+# Check name value type
+print(type(name)) 
+# # Check age value type
+print(type(age))
+
+print("Welcome, " +name+ "! Your " +age+ " Years Old !")
---
+++ 02 Basic Syntax and Variables/VariablesDataType.py
@@ -0,0 +1,19 @@
+
+# Variable Declaration 
+name = "Raju"           # String Type Variable
+age = 32                # Integer Type Variable
+height = 5.8            # Float Type Variable
+is_student = True       # Boolean Type Variable (True or False)
+
+
+# Type Conversion
+x = 5
+y = 3.8
+z = "20"
+
+a = float(x) # 5.0
+b = int(y) # 3
+c = int(z) # 4
+print(a)
+print(b)
+print(c)
